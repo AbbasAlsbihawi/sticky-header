0127c250aba9aaa5444c0df8332cf3c4eff8ae0f
Abbas Alsbihawi <abbas202559@gmail.com>
2024-05-06 09:58:08 +0300

    Initial Commit

---
+++ sticky header/sticky_headerApp.swift
@@ -0,0 +1,17 @@
+//
+//  sticky_headerApp.swift
+//  sticky header
+//
+//  Created by PC Abbas Workspace on 06/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct sticky_headerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
